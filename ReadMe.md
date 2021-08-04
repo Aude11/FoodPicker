2a9931345b10596ba532b966789aa91912354736
Aude11 <72932493+Aude11@users.noreply.github.com>
2021-08-05 00:18:28 +0100

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,77 @@
+# Food Picker Website
+
+## Overview
+
+Dynamic and responsive website aiming to help users to decide what to eat or drink.
+User can register to the website and save the city where they are or live and select their diet preferences for a more tailor experience. OpenWeather API is used to query the weather description and temperature (in degree) of the city entered in order to tailor the meals and drinks option according to it.
+
+The website has been deployed on Heroku.
+
+## Technologies
+
+* Front-end :
+  * HTML
+  * CSS
+  * Javascript
+  * Jquery
+  * Boostrap - version 5.0
+
+* Back-end :
+  * Node.JS - version 14.17
+  * ejs
+  * moongose
+
+* DataBase:
+  * MongoDB - version 4.4.5
+
+* API:
+  * [OpenWeather API](https://openweathermap.org/api)
+
+## Usage
+
+On the home page, scroll down and click either on the button next to feeling hungry section to get a meal. Or if you are feeling thirsty, click on the button next to this section.
+For a customised experience, go to the login page and register. At the registration stage, enter a username and a password (both are required) then press submit. The preference page is then loaded, fill the user preference form by entering the city where you live or currently are, and selection your diets (several diets can be selected.
+The user data are saved on MongoDB database.
+
+**Start the program**
+```
+node app.js
+```
+
+## Images
+
+![Home Page](public/images/Home-page.png)
+
+![Button Page](public/images/Button.png)
+
+![Log Page](public/images/Log-page.png)
+
+![Preference Setup Page](public/images/Setup-page.png)
+
+
+## Security
+
+The passwords will be encrypted and decrypted transparently with moongose-encrypter during save and find.
+Passwords are hashed using bcrypt password-hashing function
+For security reason, please DO NOT choose a password that you are using for sensitive content.
+
+To read more check this post from Coda Hale: https://codahale.com/how-to-safely-store-a-password/
+
+
+### Credit
+
+Icons made by [Freepik](https://www.freepik.com) Freepik from [Flaticon](https://www.flaticon.com)  www.flaticon.com
+
+##Status
+
+### ToDo:
+
+* Perform tests and write unit test
+* Add feature to enable users to update their city and preferences
+* add feature to handle the case when several users use the same username
+* Add feature to allow users to delete their account
+* Add country feature to proper location of the city for API
+
+### Contact
+
+Created by aude11
---
+++ app.js
@@ -0,0 +1,217 @@
+// jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const bcrypt = require("bcrypt");
+//const md5 = require('md5'); // to get hash function
+
+const saltRounds = 10; // at least 10 add salt to password
+
+const apiKey =  process.env.APIKEY;
+const admin = process.env.ADMIN_USERNAME;
+const passwordDB = process.env.PASSWORD;
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?appid=";
+
+let vegan = ["Avocado Toast", "Protein salad", "Lasagne vegan", "Ratatouille", "Salad of quinoa", "Vegan Maki", "Vegan pizza", "Tomato pie", "Mushroom burger", "Roasted Veggie"];
+let veggie = vegan.concat(["Risotto", "Pasta pesto", "Cheese and Bread", "Veggie Pizza", "Scramble egg", "Leek pie", "Salty cake"]);
+let healthy = ["Sushi", "Acai", "Avocado Toast", "Protein salad", "Scramble egg", "Smoothie", "Protein Bowl", "Chicken salad", "Lasagne vegan", "Ratatouille", "Salad of quinoa", "Vegan Maki"];
+let classicMeal = ["Paella", "Fish and Chips", "Sunday roast", "Burger", "Dahl", "Pad Thai", "Miso Soup"];
+let allMeal = veggie.concat(classicMeal);
+
+let queryDefault = "London";
+let defaultCity = queryDefault + " (default city as the input city does not match any cites)"
+let units = "metric";
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb+srv://"+admin+":"+passwordDB+"@cluster0.ndyoh.mongodb.net/foodDB",{
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}); // server database
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  city: String,
+  preferences: []
+});
+
+const User = mongoose.model("User", userSchema);
+
+const user = {
+  username: "",
+  city: "",
+  preferences: []
+};
+
+app.post('/register', function(req, res) {
+  user.username = req.body.username;
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      username: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render('preferences');
+      } else {
+        console.log(err);
+        res.redirect("/log");
+      }
+    });
+  });
+});
+
+app.post('/preferences', function(req, res) {
+
+  user.city = req.body.city;
+  if (req.body.vegan === "vegan") {
+    user.preferences = vegan;
+  }
+  if (req.body.veggie === "veggie") {
+    user.preferences = veggie;
+  }
+  if (req.body.healthy === "healthy") {
+    user.preferences = healthy;
+  }
+  if (req.body.healthy === "healthy" & user.preferences.length !== 0) {
+    user.preferences = healthy.filter(value => user.preferences.includes(value));
+  }
+  if (user.preferences.length === 0) {
+    user.preferences = allMeal;
+  }
+
+  User.updateOne({
+    username: user.username
+  }, {
+    city: user.city,
+    preferences: user.preferences
+  }, function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect("/log");
+    }
+  });
+
+  let query = user.city;
+  let url = apiUrl + apiKey + "&q=" + query + "&units=" + units;
+  https.get(url, function(response) {
+    if (response.statusCode === 200) {
+      parseWeatherRender(res, response, user.city, user);
+    } else {
+      console.log(response.statusCode);
+      let url = apiUrl + apiKey + "&q=" + queryDefault + "&units=" + units;
+      https.get(url, function(response) {
+        parseWeatherRender(res, response, defaultCity, user);
+      });
+    }
+  });
+});
+
+app.post("/log-in", function(req, res) {
+  User.find({
+      username: req.body.usernameLog
+    },
+    function(err, userFound) {
+      if (err) {
+        console.log(err);
+        res.redirect("/");
+      }
+      if (userFound.length !== 0) {
+        bcrypt.compare(req.body.passwordLog, userFound[0].password, function(err, result) {
+          if (result === true) {
+            let query = userFound[0].city;
+            let url = apiUrl + apiKey + "&q=" + query + "&units=" + units;
+            https.get(url, function(response) {
+              if (response.statusCode === 200) {
+                parseWeatherRender(res, response, userFound[0].city, userFound[0]);
+              } else {
+                console.log(response.statusCode);
+                let url = apiUrl + apiKey + "&q=" + queryDefault + "&units=" + units;
+                https.get(url, function(response) {
+                  parseWeatherRender(res, response, defaultCity, userFound[0]);
+                });
+              }
+            });
+          }
+        });
+      } else {
+        res.redirect("/log");
+      }
+    });
+});
+
+
+app.get('/login-home', function(req, res) {
+  let query = user.city;
+  let url = apiUrl + apiKey + "&q=" + query + "&units=" + units;
+  https.get(url, function(response) {
+    if (response.statusCode === 200) {
+      parseWeatherRender(res, response, user.city, user);
+    } else {
+      console.log(response.statusCode);
+      let url = apiUrl + apiKey + "&q=" + queryDefault + "&units=" + units;
+      https.get(url, function(response) {
+        parseWeatherRender(res, response, defaultCity, user);
+      });
+    }
+  });
+});
+
+
+app.get('/preferences', function(req, res) {
+  res.render('preferences');
+});
+
+app.get("/", function(req, res) {
+  res.render('home');
+});
+
+app.get("/log", function(req, res) {
+  res.render('log');
+});
+
+app.get("/log-in", function(req, res) {
+  res.render('log');
+});
+
+app.get("/login.html", function(req, res) {
+  res.redirect("/log");
+});
+
+app.get("/index.html", function(req, res) {
+  res.redirect("/");
+});
+
+function parseWeatherRender(res, response, city, userLogged) {
+  response.on("data", function(data) {
+    var weatherData = JSON.parse(data);
+    var temp = weatherData.main.temp;
+    var descriptionWeather = weatherData.weather[0].description;
+    renderCustumerizePage(res, userLogged, city, temp, descriptionWeather);
+  });
+}
+
+function renderCustumerizePage(res, userLogged, city, temp, descriptionWeather) {
+  res.render('login-home', {
+    username: userLogged.username,
+    city: city,
+    temp: temp,
+    description: descriptionWeather,
+    mealOption: userLogged.preferences
+  });
+}
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server");
+});
